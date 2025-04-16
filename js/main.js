01236eb08528083080b5c2d2b3474bbf56f36937
keroppi <yejiseo0725@gmail.com>
2025-04-16 17:19:16 +0900

    header_animation & scroll event

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(function () {
   Splitting();
 });
+
+$(function () {
+  let prevScrollTop = 0;
+
+  document.addEventListener("scroll", function () {
+    let nowScrollTop = $(window).scrollTop();
+
+    if (nowScrollTop > prevScrollTop) {
+      $("header").addClass("active");
+    } else {
+      $("header").removeClass("active");
+    }
+    prevScrollTop = nowScrollTop;
+  });
+});
