91aac1d281c9db4b3eafad2c046a38af74b711b2
keroppi <yejiseo0725@gmail.com>
2025-05-05 21:44:59 +0900

    visual_gsap_example

--- js/main.js
+++ js/main.js
@@ -16,3 +16,49 @@ $(function () {
     prevScrollTop = nowScrollTop;
   });
 });
+
+// ex
+window.onload = function () {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".ex",
+        start: "30% 55%",
+        end: "100% 0%",
+        scrub: 1,
+        markers: true,
+      },
+    })
+    .to(
+      ".logoWrap #j",
+      { x: "20", y: "1450", rotate: "40", ease: "none", duration: 7 },
+      0
+    )
+    .to(
+      ".logoWrap #y",
+      { x: "20", y: "1120", rotate: "-50", ease: "none", duration: 7 },
+      1
+    )
+    .to(
+      ".logoWrap #o",
+      { x: "45", y: "1350", rotate: "50", ease: "none", duration: 7 },
+      1.3
+    )
+    .to(
+      ".logoWrap #u",
+      { x: "55", y: "1350", rotate: "80", ease: "none", duration: 7 },
+      0
+    )
+    .to(
+      ".logoWrap #n",
+      { x: "100", y: "1200", rotate: "40", ease: "none", duration: 7 },
+      0.5
+    )
+    .to(
+      ".logoWrap #g",
+      { x: "140", y: "1100", rotate: "-80", ease: "none", duration: 7 },
+      0.2
+    );
+};
