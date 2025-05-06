92a9fbe6358f26a8a492db4758d53edd8c64a713
keroppi <65063305+yejiseo0725@users.noreply.github.com>
2025-05-06 23:39:44 +0900

    about

--- js/main.js
+++ js/main.js
@@ -24,41 +24,46 @@ window.onload = function () {
   gsap
     .timeline({
       scrollTrigger: {
-        trigger: ".ex",
-        start: "30% 55%",
+        trigger: ".visual",
+        start: "100% 100%",
         end: "100% 0%",
         scrub: 1,
-        markers: true,
+        // markers: true,
       },
     })
     .to(
-      ".logoWrap #j",
-      { x: "20", y: "1450", rotate: "40", ease: "none", duration: 7 },
+      ".logoWrap #y",
+      { x: "20", y: "1750", rotate: "40", ease: "none", duration: 7 },
       0
     )
     .to(
-      ".logoWrap #y",
-      { x: "20", y: "1120", rotate: "-50", ease: "none", duration: 7 },
-      1
+      ".logoWrap #e",
+      { x: "20", y: "1820", rotate: "-50", ease: "none", duration: 7 },
+      0.2
+    )
+    .to(
+      ".logoWrap #j",
+      { x: "45", y: "1770", rotate: "50", ease: "none", duration: 7 },
+      0.4
+    )
+    .to(
+      ".logoWrap #i",
+      { x: "55", y: "1760", rotate: "80", ease: "none", duration: 7 },
+      0.1
+    )
+    .to(
+      ".logoWrap #s",
+      { x: "20", y: "1850", rotate: "40", ease: "none", duration: 7 },
+      0
+    )
+    .to(
+      ".logoWrap #e",
+      { x: "20", y: "1920", rotate: "-50", ease: "none", duration: 7 },
+      0.2
     )
     .to(
       ".logoWrap #o",
-      { x: "45", y: "1350", rotate: "50", ease: "none", duration: 7 },
-      1.3
-    )
-    .to(
-      ".logoWrap #u",
-      { x: "55", y: "1350", rotate: "80", ease: "none", duration: 7 },
-      0
-    )
-    .to(
-      ".logoWrap #n",
-      { x: "100", y: "1200", rotate: "40", ease: "none", duration: 7 },
-      0.5
-    )
-    .to(
-      ".logoWrap #g",
-      { x: "140", y: "1100", rotate: "-80", ease: "none", duration: 7 },
-      0.2
+      { x: "45", y: "1870", rotate: "50", ease: "none", duration: 7 },
+      0.4
     );
 };
