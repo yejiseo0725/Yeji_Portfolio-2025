679d948b288c72b039376e2d34c2713060a758a5
keroppi <yejiseo0725@gmail.com>
2025-06-11 17:34:51 +0900

    acc list js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+$(".accordion ul li .acc-title").on("click", function () {
+  const li = $(this).closest("li");
+  const desc = $(this).next(".acc-desc");
+
+  if (desc.is(":visible")) {
+    // 닫힐 때
+    desc.removeClass("animate-in"); // 애니메이션 제거
+    desc.removeClass("open");
+    desc.slideUp(200);
+  } else {
+    // 열릴 때
+    desc.stop(true, true).slideDown(200, function () {
+      // 슬라이드 완료 후 애니메이션 클래스 추가
+      desc.addClass("animate-in");
+      li.addClass("open");
+    });
+  }
+});
+
 $(function () {
   $(".my-skill").slick({
     centerMode: true,
